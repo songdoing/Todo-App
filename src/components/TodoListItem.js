671cf375ae8546c51f0dc16e069e9f2b24de8160
wonjinsong <msmk22239@gmail.com>
2020-11-29 16:45:56 -0500

    todo listitem

---
+++ src/components/TodoListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MeCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import './TodoItems.scss';
+
+const TodoListItems = () => {
+    return (
+        <div className="TodoListItem">
+            <div className="checkbox">
+                <MdCheckBoxOutlineBlank/>
+                <div className="text">WHAT TO DO</div>
+            </div>
+            <div className="remove">
+                <MdRemoveCircleOutline/>
+            </div>
+        </div>
+    );
+};
+
+export default TodoListItems;
