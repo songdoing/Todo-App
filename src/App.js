a77e78cf4c4024b2ce707d1b1b66a6f6168f7498
wonjinsong <msmk22239@gmail.com>
2020-12-05 20:36:34 -0500

    performance improved- React.memo, functional update

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
-import React, { useState, useRef, useCallback } from 'react';
-import TodoTemplate from './components/TodoTemplate';
-import TodoInsert from './components/TodoInsert';
-import TodoList from './components/TodoList';
+import React, { useState, useRef, useCallback } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   const [todos, setTodos] = useState([
     {
-      id : 1,
-      text : 'study react',
-      checked : true,
+      id: 1,
+      text: "study react",
+      checked: true,
     },
     {
-      id : 2,
-      text : 'component styling',
-      checked : true,
+      id: 2,
+      text: "component styling",
+      checked: true,
     },
     {
-      id : 3,
-      text : 'todo App',
-      checked : false,
+      id: 3,
+      text: "todo App",
+      checked: false,
     },
   ]);
   const nextId = useRef(4);
 
-  const onInsert =useCallback(
-    text => {
+  const onInsert = useCallback(
+    (text) => {
       const todo = {
-        id : nextId.current,
+        id: nextId.current,
         text,
-        checked : false,
+        checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos((todos) => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos],
+    [todos]
   );
 
   const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
+    (id) => {
+      setTodos((todos) => todos.filter((todo) => todo.id !== id));
     },
-    [todos],
+    [todos]
   );
 
   const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? {...todo, checked : !todo.checked} : todo,
-        ),
+    (id) => {
+      setTodos((todos) =>
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
       );
     },
-    [todos],
+    [todos]
   );
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos = {todos} onRemove ={onRemove} onToggle={onToggle} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
-  );  
+  );
 };
 
 export default App;
